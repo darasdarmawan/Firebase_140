f98b435f37ad00355683a1b4096e5c08e493d1d1
darasdarmawan <darasyauqidarmawan@gmail.com>
2026-01-02 20:07:14 +0700

    menambahkan fungsi loadSiswa

--- app/src/main/java/com/example/firebase/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/HomeViewModel.kt
@@ -4,8 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.firebase.modeldata.Siswa
 import com.example.firebase.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed interface StatusUiSiswa {
     data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
@@ -17,5 +20,18 @@ class HomeViewModel(private val repositorySiswa: RepositorySiswa): ViewModel() {
         private set
     init {
         loadSiswa()
+    }
+    fun loadSiswa(){
+        viewModelScope.launch {
+            statusUiSiswa = StatusUiSiswa.Loading
+            statusUiSiswa = try {
+                StatusUiSiswa.Success(repositorySiswa.getDataSiswa())
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            }
+            catch (e: Exception){
+                StatusUiSiswa.Error
+            }
+        }
     }
 }
